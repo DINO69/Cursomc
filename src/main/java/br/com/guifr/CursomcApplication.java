ff58184c033cf12f4e787bbe8371ff9b65776dea
GUILHERME FERNANDES <guifrfr@gmail.com>
2019-05-08 20:22:26 -0300

    Criado operacao de instanciacao

--- src/main/java/br/com/guifr/CursomcApplication.java
+++ src/main/java/br/com/guifr/CursomcApplication.java
@@ -1,13 +1,34 @@
 package br.com.guifr;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.guifr.domain.CategoriaDomain;
+import br.com.guifr.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		CategoriaDomain cat1 = new CategoriaDomain(null, "Informática");
+		CategoriaDomain cat2 = new CategoriaDomain(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));		
+		
+		
 	}
 
 }
