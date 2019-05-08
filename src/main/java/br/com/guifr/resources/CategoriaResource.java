dc2ac1d9382b4594c77efb114c91ce3d23a40e5e
GUILHERME FERNANDES <guifrfr@gmail.com>
2019-05-07 21:34:31 -0300

    Testando a primeira classe de dominio

--- src/main/java/br/com/guifr/resources/CategoriaResource.java
+++ src/main/java/br/com/guifr/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.guifr.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.guifr.domain.CategoriaDomain;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<CategoriaDomain> listar() {
+		CategoriaDomain cat1 = new CategoriaDomain(1,"Informática");
+		CategoriaDomain cat2 = new CategoriaDomain(2,"Escritório");
+		
+		List<CategoriaDomain> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 	
