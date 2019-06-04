982369c1a4e229863c6b4725371a35ca59eb20f7
GUILHERME FERNANDES <guifrfr@gmail.com>
2019-06-03 21:06:03 -0300

    Arrumando as coisasgit status! Branch da Unisys

--- src/main/java/br/com/guifr/CursomcApplication.java
+++ src/main/java/br/com/guifr/CursomcApplication.java
@@ -148,6 +148,8 @@ public class CursomcApplication implements CommandLineRunner {
 		ped2.getItens().addAll(Arrays.asList(ip3));
 		
 		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
 		
 		itemRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
