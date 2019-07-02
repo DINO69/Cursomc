9283932c414570dac4dd51366b1774933f27e261
GUILHERME FERNANDES <guifrfr@gmail.com>
2019-07-01 22:58:39 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/guifr/config/DevConfig.java
@@ -0,0 +1,35 @@
+package br.com.guifr.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.guifr.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instatiateDatabase() throws ParseException {
+		
+		if(!strategy.equals("create")) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+		
+	}
+	
+}
